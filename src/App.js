83538e9d654d7bb88aae1bad2d9fd238ba4cadf4
Amirreza <amirreza.m13825@gmail.com>
2023-01-26 20:59:39 +0330

    discussion comp created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Discussion from "./container/Discussion/Discussion";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Discussion />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/container/Discussion/Discussion.jsx
@@ -0,0 +1,11 @@
+const Discussion = () => {
+  return (
+    <main>
+      <section>comment</section>
+      <section>full comment</section>
+      <section>add newcomment</section>
+    </main>
+  );
+};
+
+export default Discussion;
