e98929f23ea39c55c30b5d4a764b3274ab86fde6
Amirreza <amirreza.m13825@gmail.com>
2023-01-27 17:18:44 +0330

    FullComment component developed

--- src/components/FullComment/FullComment.jsx
+++ src/components/FullComment/FullComment.jsx
@@ -1,13 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./fullComment.css";
 
 const FullComment = ({ commentId }) => {
-  return (
-    <div className="fullComment">
-      <p>User Name</p>
-      <p>Email</p>
-      <p>Body</p>
-    </div>
-  );
+  const [comment, setComment] = useState(null);
+
+  useEffect(() => {
+    if (commentId) {
+      (async function () {
+        try {
+          const { data } = await axios.get(
+            `https://jsonplaceholder.typicode.com/comments/${commentId}`
+          );
+
+          setComment(data);
+        } catch (error) {}
+      })();
+    }
+  }, [commentId]);
+
+  // Styles
+  const h3Styles = {
+    textAlign: "center",
+    margin: "1rem 0",
+    padding: "15px",
+    color: "#444",
+    backgroundColor: "#efefef",
+    borderRadius: "5px",
+  };
+
+  // Conditional rendering
+  let commentDetail = <h3 style={h3Styles}>Please select a comment !</h3>;
+
+  if (commentId) commentDetail = <h3 style={h3Styles}>Loading...</h3>;
+
+  if (comment)
+    commentDetail = (
+      <div className="fullComment">
+        <p>
+          <span>Name: </span>
+          {comment.name}
+        </p>
+        <p>
+          <span>Email: </span>
+          {comment.email}
+        </p>
+        <p>{comment.body}</p>
+      </div>
+    );
+
+  return commentDetail;
 };
 
 export default FullComment;
