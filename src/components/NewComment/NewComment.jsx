5644f57a6c387268554926101124ad89e16d7a2f
Amirreza <amirreza.m13825@gmail.com>
2023-01-27 17:25:04 +0330

    NewComment component styles improved and Add button added

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -3,18 +3,20 @@ import "./newComment.css";
 const NewComment = () => {
   return (
     <div className="newComment">
-      <div className="inputDiv">
+      <h2>Add New Comment</h2>
+      <div className="formControl">
         <label>Name</label>
         <input type="text" />
       </div>
-      <div className="inputDiv">
+      <div className="formControl">
         <label>Email</label>
         <input type="email" />
       </div>
-      <div className="inputDiv">
+      <div className="formControl">
         <label>Body</label>
         <textarea></textarea>
       </div>
+      <button className="addBtn">Add</button>
     </div>
   );
 };
