47eff83b5bb3f6d7dc783e7253865128a8af1dc1
Amirreza <amirreza.m13825@gmail.com>
2023-01-31 23:15:12 +0330

    axios interceptor added

--- src/Services/httpService.js
+++ src/Services/httpService.js
@@ -3,6 +3,28 @@ import axios from "axios";
 // Base URL
 axios.defaults.baseURL = "https://json-server-vercel-jade-beta.vercel.app/api";
 
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
 const http = {
   get: axios.get,
   put: axios.put,
