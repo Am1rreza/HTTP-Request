47867b3576bed83b71b51616cd1c3ff55e140f6d
Amirreza <amirreza.m13825@gmail.com>
2023-02-04 00:28:47 +0330

    Switch and Layout added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Discussion from "./container/Discussion/Discussion";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <ToastContainer/>
-      <Discussion />
-    </div>
+    <React.StrictMode>
+      <div className="App">
+        <ToastContainer />
+        <Layout>
+          <Switch>
+            {routes.map((route, index) => {
+              return <Route key={index} {...route} />;
+            })}
+          </Switch>
+        </Layout>
+      </div>
+    </React.StrictMode>
   );
 }
 
